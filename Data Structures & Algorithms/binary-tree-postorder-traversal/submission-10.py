d361d8c8376fd9c0ed9633f617e89347809f937e
Ashish Kurse <98429783+ashcodex505@users.noreply.github.com>
2026-08-28 20:00:40 -0700

    Add: binary-tree-postorder-traversal - submission-10

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-10.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        #first we did recursvely then we do iteratively 
+
+        # bfs - frist in frist out 
+        # dfs - first in last out 
+        # we are using dfs but we need to explore all the child nodes of the current node for it to be added to our traversal list so that is the codniton 
+       
+        #recursively 
+        res = []
+
+        
+        def dfs(current):
+            nonlocal res
+            
+            if current and current.left == None and current.right == None:
+                res.append(current.val)
+                return 
+            
+            if current == None:
+                return 
+            
+            dfs(current.left)
+            dfs(current.right)
+            res.append(current.val)
+
+
+
+        dfs(root)
+     
+        return res
