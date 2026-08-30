d8d35d473e8f147473ef77c8d9c8c9690e32ebce
Ashish Kurse <98429783+ashcodex505@users.noreply.github.com>
2026-08-29 20:03:21 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        strMap = defaultdict(list) #hashmap with value eing defaults to arrays
+
+        #array for len 26 and put in a set each array  
+        for s in strs:
+            
+            char = [0] * 26
+            for i in s: 
+                char[ord(i) - ord("a")] += 1 
+            
+
+            char = tuple(char)
+            
+            strMap[char].append(s)
+        res = []
+        for k, v in strMap.items():
+            curr = []
+            for s in v: 
+                curr.append(s)
+            res.append(curr)
+        
+        return res 
+
+
+
+
+
+
